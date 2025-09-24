c8357af50bd65c4d5f189c60a2d4eca782dff3cc
Christian Michell <contacto@christianmichell.cl>
2025-09-24 19:56:08 -0300

    Initial Commit

---
+++ IAhorro/IAhorroApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAhorroApp.swift
+//  IAhorro
+//
+//  Created by Christian Michell on 24-09-25.
+//
+
+import SwiftUI
+
+@main
+struct IAhorroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
